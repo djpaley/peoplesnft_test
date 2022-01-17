4b78d21eb7a852e3f18c0530e4f3b4ef728952a1
David Paley <djpaley@gmail.com>
2022-01-17 08:57:08 -0600

    adding roadmap and team page

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+function Roadmap() {
+    return (
+        <div className="container bootstrap snippets bootdeys">
+            <div className="col-md-9">
+                <div className="timeline-centered timeline-sm">
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 1</span>
+                                <strong>Mint</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>An NFT that commemorates the ConstitutionDAO effort & onboards minters to lead the future of the project</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 2</span>
+                                <strong>Vote</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>On a historically significant artifact by NFT-holding contributors</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 3</span>
+                                <strong>Purchase</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>Artifacts in the physical world</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 4</span>
+                                <strong>Unveil</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>Web3's first public exhibit to the world that educates the audience on the artifacts history and about the web3 ecosystem</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 5</span>
+                                <strong>Grow</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>The contributor community & acquire more artifacts to expand the educational mission</p>
+                            </div>
+                        </div>
+                    </article>
+                    <article className="timeline-entry">
+                        <div className="timeline-entry-inner">
+                            <div className="timeline-phase">
+                                <span>Phase 6</span>
+                                <strong>Enable</strong>
+                            </div>
+                            <div className="timeline-icon">IMAGE</div>
+                            <div className="timeline-description">
+                                <p>Priority exhibit access for NFT contributors and host public events to grow community awareness</p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Roadmap;
